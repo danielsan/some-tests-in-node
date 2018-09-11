b83dbf4bd8992d65a098754f9bbc5785e2f5b896
Daniel Santana <github@danielsantana.net>
2018-09-11 17:21:32 -0400

    improves the tests

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 
-const getCorrectIpAddress = privateIp => (process.env.FORCE_LOCAL_IP ? require('ip').address() : privateIp);
+// const getCorrectIpAddress = privateIp => (process.env.FORCE_LOCAL_IP ? require('ip').address() : privateIp);
 
-// const ip = require('ip');
+const ip = require('ip');
 
-// function getCorrectIpAddress(privateIp) {
-//   if (process.env.FORCE_LOCAL_IP) {
-//     return ip.address();
-//   } else {
-//     return privateIp;
-//   }
-// }
+const { sayDarielIsMyCompadre } = require('./say-dariel-is-my-compadre');
+// const sayDarielIsMyCompadre = () => {
+//   process.stdout.write('Dariel is my Compadre!!!\n')
+// };
+
+function getCorrectIpAddress(privateIp) {
+  sayDarielIsMyCompadre();
+  const { FORCE_LOCAL_IP } = process.env;
+  // console.log({ FORCE_LOCAL_IP })
+  if (process.env.FORCE_LOCAL_IP) {
+    return ip.address();
+    // return '10.0.1.216';
+  } else {
+    return privateIp;
+  }
+}
 
 // const getCorrectIpAddress = (someIp, x) => {
 //   console.log(typeof process.env.FORCE_LOCAL_IP, x);
 //   return process.env.FORCE_LOCAL_IP ? null : someIp;
 // }
 
-module.exports = { getCorrectIpAddress };
+module.exports = { getCorrectIpAddress, sayDarielIsMyCompadre };
--- test/index.test.js
+++ test/index.test.js
@@ -1,32 +1,67 @@
 
 const { expect } = require('chai');
+const sinon = require('sinon');
+const cleanrequire = require('@everymundo/cleanrequire');
 
 describe('index.js', () => {
+  let box;
+  beforeEach(() => { box = sinon.createSandbox(); });
+  afterEach(() => { box.restore(); });
+
   describe('#getCorrectIpAddress', () => {
     context('When the FORCE_LOCAL_IP env var exists', () => {
+      const ip = require('ip');
+      const lib = require('../say-dariel-is-my-compadre');
+      // const lib = require('../');
+      const myIp = 'ip.address()' + Math.random();
+
       beforeEach(() => {
-        process.env.FORCE_LOCAL_IP = 'true';
+        box.stub(ip, 'address').callsFake(() => myIp);
+        box.stub(lib, 'sayDarielIsMyCompadre');
+        // box.stub(process.env, 'FORCE_LOCAL_IP').value('true');
+        // process.env.FORCE_LOCAL_IP = 'true';
       });
 
       it('should return the the REAL ip address', () => {
-        const { getCorrectIpAddress } = require('../index.js');
+        const { getCorrectIpAddress } = cleanrequire('../index.js');
 
         const myPresetIpAddress = '0.0.0.0';
-        const expected = require('ip').address();
+        const expected = ip.address();
 
         const result = getCorrectIpAddress(myPresetIpAddress);
 
         expect(result).to.equal(expected);
       });
 
-      afterEach(() => {
-        delete process.env.FORCE_LOCAL_IP;
-      })
+      it('should call the address method of the ip package', () => {
+        const { getCorrectIpAddress } = cleanrequire('../index.js');
+
+        const myPresetIpAddress = '0.0.0.0';
+
+        getCorrectIpAddress(myPresetIpAddress);
+
+        expect(ip.address).to.have.property('calledOnce', true);
+      });
+
+      it('should call the address the function sayDarielIsMyCompadre', () => {
+        const { getCorrectIpAddress } = cleanrequire('../index.js');
+
+        const myPresetIpAddress = '0.0.0.0';
+
+        getCorrectIpAddress(myPresetIpAddress);
+
+        expect(lib.sayDarielIsMyCompadre).to.have.property('calledOnce', true);
+      });
     });
 
     context('When FORCE_LOCAL_IP env var does NOT exists', () => {
+      let previousValue;
+      beforeEach(() => {
+        box.stub(process.env, 'FORCE_LOCAL_IP').value('');
+      });
+
       it('should return the same value passed as argument', () => {
-        const { getCorrectIpAddress } = require('../index.js');
+        const { getCorrectIpAddress } = cleanrequire('../index.js');
 
         const myPresetIpAddress = '0.0.0.0';
 
